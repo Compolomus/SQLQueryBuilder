cb72e0871508c70ebff2bcc062986a7e62fae7c7
compolom <compolom@gmail.com>
2017-06-06 22:21:39 +0200

    small fix #4

--- src/System/Traits/PDOInstance.php
+++ src/System/Traits/PDOInstance.php
@@ -6,15 +6,19 @@ trait PDOInstance
 {
     private $db;
 
-    public function getPDO($config = null) {
+    private $config;
+
+    public function getPDO($config = [])
+    {
         /* Here its own implementation of the interface */
+        $this->config = $config;
         $dbHost = 'localhost';
         $dbName = 'git';
         $dbUser = 'root';
         $dbPass = '';
         $this->db = new \PDO('mysql:host=' . $dbHost . ';dbname=' . $dbName, $dbUser, $dbPass,
             [
-                \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
                 \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
                 \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4'",
                 \PDO::MYSQL_ATTR_USE_BUFFERED_QUERY,
@@ -22,7 +26,8 @@ trait PDOInstance
         );
     }
 
-    public function db() {
+    public function db()
+    {
         return $this->db;
     }
 }
